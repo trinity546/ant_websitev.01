74cbfbeec2e06325e7b18e511217866c2fab36be
trinity546 <katymcintosh@katys-mbp.wifi.bcit.ca>
2016-02-25 15:28:37 -0800

    added name

--- products.php
+++ products.php
@@ -23,11 +23,11 @@ loadScripts();
                         <img src='$img'  class='img-responsive img-hover'>
                     </div>
                     <div class='col-md-5'>
-                        <h3 data-sku-desc='$sku'>$name</h3>
+                        <h3 data-sku-name='$sku'>$name</h3>
                         <p data-sku-desc='$sku'>$desc</p>
                         <input data-sku-qty='$sku' type='number' value='1' min='1' max='10' step='1'/>
                         <h4 data-sku-price='$sku'>$price</h4>
-                        <a href='#myAnchor' onclick='smoothScroll(document.getElementById('myAnchor'))''><input data-sku-add='$sku'                                 type='button' class='startCart btn btn-primary' value='Add To Cart '/></a>      
+                        <a ><input data-sku-add='$sku' type='button' class='startCart btn btn-primary' value='Add To Cart '/></a>      
                 </div>  
                 </div>
                 <hr>
